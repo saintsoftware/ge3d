9c8de4d199b21c9a94adbe9e566347d088c0da3d
jdc <general@saintsoftware.eu.pn>
2014-05-27 20:43:23 +0100

    basic layout

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "image/image_lib.h"
+// Under Develop at the moment...
+
+/*
+	Main
+	empty loads nothing to printf
+	*Game check first since it's the main activity of players.
+*/
+	
+char module = "main_menu";
+
+int main(){ 
+	
+	if(module == "game") {
+		// Load game
+	}
+	else if(module == "main_menu") {
+		// Load Main Menu
+	}
+	else if(module == "loading_menu") {
+		// Load Loading_menu
+	}
+}		
+		
+	
+	
