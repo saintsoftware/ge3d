3ab9325b668b855212b9704b386d662327339300
jdc <general@saintsoftware.eu.pn>
2014-06-07 14:56:49 +0100

    clean up 2

--- image/image_lib.c
+++ image/image_lib.c
@@ -35,4 +35,14 @@ int img_conc_path_f(){
 }
 */
 
+char* concat(char *s1, char *s2)
+{
+    size_t len1 = strlen(s1);
+    size_t len2 = strlen(s2);
+    char *result = malloc(len1+len2+1);//+1 for the zero-terminator
+    //in real code you would check for errors in malloc here
+    memcpy(result, s1, len1);
+    memcpy(result+len1, s2, len2+1);//+1 to copy the null-terminator
+    return result;
+}
 
